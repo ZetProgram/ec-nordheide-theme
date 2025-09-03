0ddca749dc9ea2f451946a600c2b12c6e6ea20e0
Zet <xZetGamerHDx@gmail.com>
2025-09-03 12:09:49 +0200

    Change Tailwind Settings

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ add_action('wp_enqueue_scripts', function () {
 	$ver  = file_exists($path) ? filemtime($path) : null;
 
 	// 1) Tailwind (Build)
-	wp_enqueue_style('theme-tailwind', $base . '/dist/tailwind.css', [], $ver);
+	wp_enqueue_style('theme-tailwind', $base . '/assets/css/tailwind_minify.css', [], $ver);
 
 	wp_enqueue_style('theme-style', $base . '/style.css', ['theme-tailwind'], null);
 });
@@ -60,10 +60,10 @@ add_action('wp_enqueue_scripts', function () {
  */
 add_action('enqueue_block_editor_assets', function () {
 	$base = get_stylesheet_directory_uri();
-	$path = get_stylesheet_directory() . '/dist/tailwind.css';
+	$path = get_stylesheet_directory() . '/assets/css/tailwind_minify.css';
 	$ver  = file_exists($path) ? filemtime($path) : null;
 
-	wp_enqueue_style('theme-tailwind-editor', $base . '/dist/tailwind.css', [], $ver);
+	wp_enqueue_style('theme-tailwind-editor', $base . '/assets/css/tailwind_minify.css', [], $ver);
 });
 
 /**
@@ -149,7 +149,7 @@ add_action( 'after_setup_theme', 'theme_slug_setup' );
  * Editor-spezifische Styles (bestehend)
  */
 function ks_gutenberg_styles() {
-	wp_enqueue_style( 'gutenberg-css', get_theme_file_uri( '/gutenberg.css' ), false );
+	wp_enqueue_style( 'gutenberg-css', get_theme_file_uri( '/assets/css/gutenberg.css' ), false );
 }
 add_action( 'enqueue_block_editor_assets', 'ks_gutenberg_styles' );
 
