a2e9fc84161809bcbecf2ae57612666246d5db8b
Zet <xZetGamerHDx@gmail.com>
2025-09-24 18:29:01 +0200

    Update Header

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
           <?php endif; ?>
         </a>
 
-        <!-- Desktop Nav -->
+        
         <nav class="flex items-center gap-6" aria-label="<?php esc_attr_e('Hauptnavigation','ec-nordheide-theme'); ?>">
           <?php
             wp_nav_menu([
@@ -64,7 +64,7 @@
               'fallback_cb'    => false,
               'menu_class'     => 'hidden lg:flex items-center gap-6 font-medium',
               'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-              'link_before'    => '<span class="inline-block py-2 hover:color-[#6C9941] uppercase color-[#1A1A1A] lg:text-[20px] text-[18px]">',
+              'link_before'    => '<span class="inline-block py-2 hover:color-[#6C9941] uppercase color-[#1A1A1A] lg:text-[16px] text-[18px]">',
               'link_after'     => '</span>',
             ]);
           ?>
@@ -76,7 +76,9 @@
                   aria-label="<?php esc_attr_e('Menü öffnen','ec-nordheide-theme'); ?>">
               <img src="<?php echo esc_url( get_template_directory_uri() . '/img/menu_symbol.svg' ); ?>" alt="<?php esc_attr_e('Menü Icon','ec-nordheide-theme'); ?>" class="max-h-[60px] w-14 h-14"> 
           </button>
-
+          <a href="https://www.instagram.com/ecnordheide" class="inline-flex items-center justify-center w-10 h-10" aria-label="Instagram" title="Instagram">
+            <svg class="w-[80%] h-[80%]" aria-hidden="true" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
+          </a>
           <!-- Spenden Button -->
           <a href="<?php echo esc_url($cta_url); ?>" target="<?php echo esc_attr($cta_target); ?>" class="hidden xl:inline-flex items-center rounded-sm px-4 py-2 text-white bg-[#92C355] hover:bg-[#64863a] text-[20px] no-underline focus:outline-none transition uppercase">
             <?php echo esc_html($cta_text); ?>
